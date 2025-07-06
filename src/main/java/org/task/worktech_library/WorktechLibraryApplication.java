40e234381d27f45f63a0e581d26014711e6dec41
Edik <davydiantsedik@mail.ru>
2025-07-06 17:24:52 +0300

    INITIAL

---
+++ src/main/java/org/task/worktech_library/WorktechLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.task.worktech_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktechLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktechLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/task/worktech_library/WorktechLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.task.worktech_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktechLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
